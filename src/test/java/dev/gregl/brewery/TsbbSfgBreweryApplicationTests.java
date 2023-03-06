0bce524e9243a814b0a034b60fd47a983a703c6e
greg.langham <greg.langham@messagegears.com>
2023-03-06 15:19:40 -0600

    Initial commit

---
+++ src/test/java/dev/gregl/brewery/TsbbSfgBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gregl.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TsbbSfgBreweryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
